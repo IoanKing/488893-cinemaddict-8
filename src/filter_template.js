84469c16b162051a5d3f3b66c7826f46b621d5ee
Ivan <41145341+IoanKing@users.noreply.github.com>
2019-02-25 12:40:44 +0300

    Исправление замечания по читабельность кода.

--- src/filter_template.js
+++ src/filter_template.js
@@ -1,7 +1,13 @@
 /**
  * Шаблон фильтра.
  * @param {object} element Объект с данными для фильтра.
- * @param {bool} isFirst Признак первого элемента.
- * @return {string} разметка HTML блока с фильтром.
  */
-export default (element) => `<a href="#${element.slug}" class="main-navigation__item ${(element.slug === `all`) ? `main-navigation__item--active` : ``}">${element.title} ${(element.slug !== `all`) ? `<span class="main-navigation__item-count">${element.count}</span>` : ``}</a>`;
+export default (element) => `
+<a
+  href="#${element.slug}"
+  class="main-navigation__item ${(element.slug === `all`) ? `main-navigation__item--active` : ``}"
+  >
+    ${element.title} ${(element.slug !== `all`) ?
+      `<span class="main-navigation__item-count">${element.count}</span>` 
+      :``}
+</a>`;
